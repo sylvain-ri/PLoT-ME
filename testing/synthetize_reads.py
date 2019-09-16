6e8ce0dfcb05fe6af4298b9a164cc5cef84d9ef8
Earwelen <sylvainriondet@gmail.com>
2019-09-16 17:20:19 +0800

    slowly adding preprocessing of files to be able to cut genomes. Need to get the length of them, their total size and so on.

--- testing/synthetize_reads.py
+++ testing/synthetize_reads.py
@@ -10,8 +10,11 @@
 
 
 import argparse
-import subprocess
+import gzip
 import os
+from os.path import getsize, join as path_join
+import pandas as pd
+import subprocess
 
 from tqdm import tqdm
 
@@ -19,21 +22,59 @@ path_genomes   = "/home/sjriondet/Data/ncbi-2019-complete/refseq"
 path_out_reads = "/home/sjriondet/Data/synthetic_reads"
 
 
-def reads_from_genomes():
+def preprocess_genomes(folder):
+    """ Read through all genomes files to have a summary of the available data """
+    cache_file_name = "_genomes.pd"
+    # meta_file_name = ""
+    cache_file_path = path_join(folder, cache_file_name)
 
-    genomes_count = sum([len(files) for r, d, files in os.walk(path_genomes)])
+    if os.path.isfile(cache_file_path):
+        return pd.read_pickle(cache_file_path)
+    else:
+        # todo create the summary file
+        number_genomes = sum([len(files) for _, _, files in os.walk(path_genomes)])
+        for dir_path, dir_names, files in tqdm(os.walk(path_genomes), total=number_genomes):
+            for file in files:
+                if file.endswith((".fna", ".fa", ".fastq", )):
+                    file_path = path_join(dir_path, file)
+                    file_size = getsize(file_path)
+                    with gzip.open(file_path, 'rb') as f:
+                        content = f.read().decode("utf-8")
+                        content.count("\n")
+                        # what is inside, example:
+                        # b'>NC_002162.1 Ureaplasma parvum serovar 3 str. ATCC 700970, complete genome\nATGGCTAATAATTATCAAACTTTATATGATTCAGCAATAAAAAGGATTCCATACGATCTTATTTCTGATCAAGCTTATGC\nAATTCTACAAAATGCTAAAACTCATAAAGTTTGCGATGGTGTTT'
 
-    for dir_path, dir_names, files in tqdm(os.walk(path_genomes), total=genomes_count):
+        df = pd.DataFrame()
+        df.to_pickle(cache_file_path)
+        return df
+
+
+def reads_from_genomes(number_reads):
+    """ Create synthetic reads from intact genomee """
+    # todo: add a counter (like want x reads)
+
+    number_genomes = sum([len(files) for _, _, files in os.walk(path_genomes)])
+    reads
+
+
+    # todo: add a counter (like want x reads)
+    for dir_path, dir_names, files in tqdm(os.walk(path_genomes), total=number_genomes):
         print(path_out_reads)
 
 
 
+
+read_synthesizers = {"genome": reads_from_genomes,
+                     }
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run multiple classifiers')
-    parser.add_argument('folder', help='folder to check')
-    parser.add_argument('-s', '--selected_tasks', type=lambda s: {int(item) for item in s.split(',')} )
-    parser.add_argument('-z', '--gzip', help='zgip input files', action='store_true')
-    parser.add_argument('-m', '--max', help='Maximum files to run', type=int, default=0)
-    parser.add_argument('-n', '--nanopore_custom', help='Enable Nanopore customed settings (Centrifuge ONLY). ',
-                                type=lambda s: [item for item in s.split(',')][:2], )
+    parser = argparse.ArgumentParser(description='Create reads for benchmarking')
+    parser.add_argument('read_type', help='Select which kind of read you want', choices=read_synthesizers.keys())
+    # parser.add_argument('-s', '--selected_tasks', type=lambda s: {int(item) for item in s.split(',')} )
+    # parser.add_argument('-z', '--gzip', help='zgip input files', action='store_true')
+    # parser.add_argument('-m', '--max', help='Maximum files to run', type=int, default=0)
+    # parser.add_argument('-n', '--nanopore_custom', help='Enable Nanopore customed settings (Centrifuge ONLY). ',
+    #                             type=lambda s: [item for item in s.split(',')][:2], )
     args = parser.parse_args()
+
+
