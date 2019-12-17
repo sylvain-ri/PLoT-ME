707b7a121e05c288eda019da670e35e0676ae09d
Sylvain <Riondet_Sylvain_from.tp@gis.a-star.edu.sg>
2019-12-17 03:13:01 +0000

    extracted method kmer_count(), logging more precise, added general steps in the classification, added defaults choices or the script.

--- prod/classify.py
+++ prod/classify.py
@@ -89,30 +89,8 @@ class CustomRead(SeqRecord.SeqRecord):
         return getattr(self._wrapped_obj, attr)
     
     def count_kmer(self, ignore_N=True):
-        """ Count all kmers, ignore kmers with N or other undecided nucleotides 
-            Return a list of dict for each window (w=100)
-            seq: string nucleotide input
-            kmer_template: dict with all combinations of nucleotides, initialized with zeros (kmer_template["AAAA"] = 0, kmer_template["AAAC"] = 0...)
-            the new string hashing behaviour, BiopythonWarning: Using str(self.seq) to use the new behaviour
-        """
-        self.logger.info('counting kmers')
-        wrong_base = "N"*self.k
-        self.kmer_count[wrong_base] = 0
-        
-        try:
-            for i, kmer in enumerate(window(str(self.seq), self.k)):
-                try:
-                    self.kmer_count[kmer] += 1
-                except:
-                    self.kmer_count[wrong_base] += 1
-                    
-            if ignore_N:
-                self.kmer_count.pop(wrong_base)
-            return self.kmer_count
-        except Exception as e:
-            print("type error: " + str(e))
-            print(traceback.format_exc())
-            return self.kmer_count
+        """ common method """
+        seq_count_kmer(self.seq, self.kmer_count, self.k, ignore_N=True)
     
     def lda_reduce(self):
         self.logger.info('reducing dimension of kmer frequency to lda representation')
@@ -141,9 +119,8 @@ class CustomRead(SeqRecord.SeqRecord):
 class FastQClassification:
     """ For a fastq file, bin reads, classify them, and compare results """
     
-    def __init__(self, path_original_fastq, db_choice, bin_nb=10, classifier="kraken2",
-                 cores=multiprocessing.cpu_count(), folder_report=PATHS.FOLDER_REPORTS, 
-                 dry_run=True, verbose=False):
+    def __init__(self, path_original_fastq, db_choice, folder_report, bin_nb=10, classifier="kraken2", 
+                 cores=multiprocessing.cpu_count(), dry_run=True, verbose=False):
         self.logger = logging.getLogger('classify.FastQClassification')
         assert osp.isfile(path_original_fastq), FileNotFoundError(f"Didn't find original fastq {path_original_fastq}")
         
@@ -173,7 +150,6 @@ class FastQClassification:
                 self.path_binned_fastq.append((i, path_bin_i))
     
     def classify(self):
-        self.logger.info('start to classify reads :)')
         if self.classifier == "kraken2":
             if "bins" in self.db_choice:
                 self.find_binned_files()
@@ -185,6 +161,7 @@ class FastQClassification:
                 self.kraken2_standard()
                 
     def kraken2_standard(self):
+        self.logger.info('start to classify reads with kraken2 standard DB')
         self.cmd = [
             "kraken2", "--threads", f"{self.cores}",
             "--db", f"{PATHS.KRAKEN2_DB[self.db_choice]}", 
@@ -198,6 +175,7 @@ class FastQClassification:
             if self.verbose: print(results)
                 
     def kraken2_bins(self, i, file):
+        self.logger.info('start to classify reads with kraken2 10 bins DB')
         self.cmd = [
             "kraken2", "--threads", f"{self.cores}",
             "--db", f"{PATHS.KRAKEN2_DB[self.db_choice]}/{i}", 
@@ -211,6 +189,7 @@ class FastQClassification:
             if self.verbose: print(results)
             
     def kraken2_report_merging(self):
+        self.logger.info('Merging kraken2 reports')
         NotImplementedError
         
     
@@ -219,13 +198,22 @@ class FastQClassification:
                f"{self.classifier} with the DB <{self.db_choice}>"
         
 
+# #############################################################################
+# Defaults and main method
+path_fastq_comm = "/home/ubuntu/Data/Segmentation/Test-Data/Synthetic_from_Genomes/2019-12-05_100000-WindowReads_20-BacGut/2019-12-05_100000-WindowReads_20-BacGut.fastq"
 
-
-def classify_reads(path_fastq, path_report, classifier="kraken2"):
+def classify_reads(path_fastq, path_report, classifier, db):
     """ Should load a file, do all the processing """
     logger.info("let's classify reads!")
+    fastq_classifier = FastQClassification(
+        path_original_fastq=path_fastq, db_choice=db, folder_report=path_report, 
+        bin_nb=10, classifier=classifier)
     
-    
+    fastq_classifier.dry_run=False
+    fastq_classifier.db_choice = "2015-standard"
+    fastq_classifier.classify()
+    fastq_classifier.db_choice = "2015-10bins"
+    fastq_classifier.classify()
     
     logger.error('Classification not implemented yet')
 
@@ -234,15 +222,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=
         'Provided a fastq file with reads, bins the reads into defined bins ' \
         'and launch a classifier loading one bin at the time.')
-    parser.add_argument('input_fastq',        help='Input file in fastq format', type=is_valid_file)
-    parser.add_argument('output_folder',      help='Folder for output reports' , type=is_valid_directory)
-    parser.add_argument('-c', '--classifier', help='choose kraken2 or centrifuge', 
-                                              choices=PATHS.classifiers, default=PATHS.classifiers[0])
-    parser.add_argument('-d', '--database',   default='standard', help='which reference to use', choices=('standard', 'mini', ))
-    parser.add_argument('-t', '--threads',    default="10",       help='Number of threads')
+    parser.add_argument('-i', '--input_fastq',   help='Input file in fastq format', type=is_valid_file, 
+                                                 default=path_fastq_comm)
+    parser.add_argument('-o', '--output_folder', help='Folder for output reports' , type=is_valid_directory, 
+                                                 default=folder_today(PATHS.FOLDER_REPORTS))
+    parser.add_argument('-c', '--classifier',    help='choose kraken2 or centrifuge', 
+                                                 choices=PATHS.classifiers, default=PATHS.classifiers[0])
+    parser.add_argument('-d', '--database',      default='standard', help='which reference to use', choices=('standard', 'mini', ))
+    parser.add_argument('-t', '--threads',       default="10",       help='Number of threads')
+    
     args = parser.parse_args()
     logger.info(f'script called with following arguments: {args.input_fastq}, {args.output_folder}, {args.classifier}')
 
-    classify_reads(args.input_fastq, args.output_folder, classifier=args.classifier, )
+    classify_reads(args.input_fastq, args.output_folder, classifier=args.classifier, db=args.database)
 
 
