7d55ed7bb470b36cd85e97c8efb9c4d0b0fa66a3
Sylvain R <sylvainriondet@gmail.com>
2019-12-24 17:54:24 +0800

    proper docstring, added Genome class with its methods. Added the functions to write as todos

--- prod/parse_DB.py
+++ prod/parse_DB.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
-# #############################################################################
-# Sylvain @ GIS / Biopolis / Singapore
-# Sylvain Jun-Zhe RIONDET <Riondet_Sylvain_from.tp@gis.a-star.edu.sg>
-# Started on 2019-12-11
-# Reads Binning Project
-#
-# #############################################################################
-#
-#
-# Script to divide a Database into bins based on genome's k-mer frequencies
-#
+"""
+#############################################################################
+Script to divide a Database of genomes (RefSeq) and split it into bins
+according to the k-mer frequency of each genome. Needs a lot of disk space,
+and RAM according to the largest genome to process.
+
+#############################################################################
+Sylvain @ GIS / Biopolis / Singapore
+Sylvain Jun-Zhe RIONDET <Riondet_Sylvain_from.tp@gis.a-star.edu.sg>
+Started on 2019-12-11
+Reads Binning Project
+#############################################################################
+"""
+
 import argparse
+from copy import deepcopy
 import logging
 import os
 import os.path as osp
+import pandas as pd
 import re
 from time import time
 
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 from tqdm import tqdm
 
 # Import paths and constants for the whole project
 from prod.util import PATHS, FilesInDir, is_valid_directory
-from prod.bio import kmers_dic, ncbi, CustomRead
+from prod.bio import kmers_dic, ncbi, Read, seq_count_kmer
 
 
 # #############################################################################
@@ -44,6 +50,165 @@ logger.addHandler(fh)
 logger.addHandler(ch)
 
 
+class Genome:
+    """ Genome from RefSeq. Methods to split it into plasmid/genome and into segments """
+    categories = ["plasmid", "chloroplast", "scaffold", "contig",
+                  "chromosome", "complete genome", "whole genome shotgun sequence", ]
+    kmer_count_zeros = kmers_dic(4)
+
+    def __init__(self, fna_file, kmer_file, taxon, window, k=4):
+        self.path_fna = fna_file
+        self.path_kmers  = kmer_file
+        self.taxon       = taxon
+        self.window      = window
+        self.k           = k
+        # records is a dict of SeqRecord
+        self.records = {cat: [] for cat in self.categories}
+        # self.splits  = {cat: [] for cat in self.categories}
+
+    def load_genome(self):
+        """ Loop through all chromosomes/plasmids/genomes/.. and parse them into the object """
+        for record in SeqIO.parse(self.path_fna, "fasta"):
+            for cat in self.categories:
+                if cat in record.description:
+                    self.records[cat].append(record)
+                    break
+
+    def yield_genome_split(self):
+        """ Split a genome/plasmid into multiple windows, to count the k-mer
+            or to create the .fna files for kraken2-build
+        """
+        for cat in self.categories:
+            for record in self.records[cat]:
+
+                full_seq = record.seq
+                len_genome = len(full_seq)
+                for start in range(0, len_genome - self.window, self.window):
+                    end = min(start + self.window, len_genome - 1)
+                    # Include the taxonomy id, start and end of the segment into the description
+                    description = f"|kraken:taxid|{self.taxon}|s:{start}-e:{end-1}|{record.description}"
+                    segment = SeqRecord(full_seq[start:end],
+                                        record.id, record.name, description, record.dbxrefs,
+                                        record.features, record.annotations, record.letter_annotations)
+                    yield (segment, self.taxon, cat, start, end)
+
+    def count_kmers_to_df(self):
+        """ Take all splits, count the kmer distribution and save to the kmer folder as pandas DataFrame """
+        for_csv = []
+        for segment, taxon, cat, start, end in self.yield_genome_split():
+            kmer_count = seq_count_kmer(str(segment.seq), deepcopy(self.kmer_count_zeros), k=self.k)
+            for_csv.append((taxon, cat, start, end, segment.name, segment.description, self.path_fna,
+                            *kmer_count.values() ))
+        kmer_keys = list(self.kmer_count_zeros.keys())
+        df = pd.DataFrame(for_csv, columns=["taxon", "category", "start", "end", "name", "description", "fna_path"]
+                                           + kmer_keys)
+        df.taxon       = df.taxon.astype('category')
+        df.category    = df.category.astype('category')
+        df.description = df.description.astype('category')
+        df.to_pickle(self.path_kmers)
+
+
+def idkwhat():
+        if "write_fna":
+            raise NotImplementedError
+
+
+def scan_RefSeq_to_kmer_counts(scanning, folder_kmers, k=4, window=10000, stop=3, ):
+    """ Scan through RefSeq, split genomes into windows, count their k-mer, save in similar structure
+        Compatible with 2019 RefSeq format hopefully
+    """
+    start = time()
+
+    # scanning folder Class set up:
+    preset = {"root"  : [".taxon", ],
+              "target": [".kmer_count.pd", ]}
+    FilesInDir.set_defaults_parse_RefSeq(scanning, folder_kmers, preset=preset)
+    FilesInDir.folder_kmer = folder_kmers
+
+    logger.info("scanning through all genomes in refseq " + scanning)
+    for i, fastq in enumerate(FilesInDir.tqdm_scan()):
+        if osp.isfile(fastq.target_file[".kmer_count.pd"]):
+            continue
+        with open(fastq.taxon) as f:
+            taxon = int(f.read())
+        genome = Genome(fastq.abs_path, fastq.kmer_count_pd, taxon, window=window, k=k)
+        genome.load_genome()
+        genome.count_kmers_to_df()
+        if i > stop:
+            logger.warning("Early stop of the scanning")
+            break
+
+    # Ending
+    elapsed_time = time() - start
+    print(f"\n{FilesInDir.file_count_root} folders have been scanned\n"
+          f"Took {elapsed_time:,.1f}s / {elapsed_time / 60:.1f}min  to complete. "
+          f"{FilesInDir.file_count_root / elapsed_time:,.0f} genome/s")
+
+
+def combine_genome_kmer_counts(folder_kmers, path_df):
+    """ Combine single dataframes into one. Might need high memory """
+    logger.info("loading all kmer frequencies into a single file from " + folder_kmers)
+    dfs = []
+    for file in os.scandir(folder_kmers):
+        dfs.append(pd.read_pickle(file.path))
+    df = pd.concat(dfs, ignore_index=True)
+    logger.info(f"Saving dataframe to {path_df}")
+    df.to_pickle(path_df)
+
+
+def find_bins_DB(path_kmer_counts, n_parts=10):
+    """ Given a database of genomes in fastq files, split it in n segments """
+    raise NotImplementedError
+
+
+def write_split_to_bins(path_df_bins, path_db_bins):
+    pass
+
+
+def kraken_build(path_db_bins, path_bins_hash):
+    pass
+
+
+def main(folder_database, folder_intermediate_files, n_clusters, cores):
+    """ Pre-processing of RefSeq database to split genomes into windows, then count their k-mers
+        Second part, load all the k-mer counts into one single Pandas dataframe
+        Third train a clustering algorithm on the k-mer frequencies of these genomes' windows
+        folder_database          : RefSeq root folder
+        folder_intermediate_files: empty root folder to store kmer counts
+    """
+    scan_RefSeq_to_kmer_counts(folder_database, folder_intermediate_files, stop=3)
+    path_kmer_counts = osp.join(folder_intermediate_files, "_all_counts.kmer.pd")
+    combine_genome_kmer_counts(folder_intermediate_files, path_kmer_counts)
+    # todo: find bins and write split genomes into bins
+    path_df_bins = "todo"
+    find_bins_DB(path_kmer_counts, path_df_bins)
+    path_db_bins = "todo"
+    write_split_to_bins(path_df_bins, path_db_bins)
+    path_bins_hash = "todo"
+    kraken_build(path_db_bins, path_bins_hash)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('path_database', type=is_valid_directory,
+                        help='Database root folder. Support format: RefSeq 2019.')
+    parser.add_argument('path_intermediate_files', type=is_valid_directory,
+                        help='Folder for the k-mer counts per genome and for clustering models')
+    parser.add_argument('-c', '--clusters', default=10, type=int, help='Number of clusters to split the DB into')
+    parser.add_argument('-t', '--threads',  default="10", help='Number of threads')
+    args = parser.parse_args()
+
+    main(folder_database=args.path_database, folder_intermediate_files=args.path_intermediate_files,
+         n_clusters=args.clusters, cores=args.threads)
+    print("Not implemented yet")
+        
+
+
+
+
+
+#
+# Deprecated method
 def kmer_pkl_path(kmer_folder, fna_path, taxo_ext="gff"):
     """ Legacy method, might not use it anymore in the future
         Return a file name based on the taxonomy id instead of the file name.
@@ -97,57 +262,7 @@ def kmer_pkl_path(kmer_folder, fna_path, taxo_ext="gff"):
     return taxo, bacteria_name, fna_name, out_path
 
 
-def scan_RefSeq_to_kmer_counts(scanning, folder_kmers, k=4, window=10000, stop=3, ):
-    """ Scan through RefSeq, split genomes into windows, count their k-mer, save in similar structure
-        Compatible with 2019 RefSeq format hopefully
-    """
-    start = time()
-
-    # scanning folder Class set up:
-    preset = {"root"  : [".taxon", ],
-              "target": [".kmer_count.pd", ]}
-    FilesInDir.set_defaults_parse_RefSeq(scanning, folder_kmers, preset=preset)
-
-    dic_template = {"bacteria": "", "fna": "", "start": None,}
-
-    for fastq in FilesInDir.tqdm_scan():
-        if osp.isfile(fastq.target_file[".kmer_count.pd"]):
-            continue
-        genome = SeqIO.parse(fastq.abs_path, "fasta")
-
-
-        # try:
-        #     # Count
-        #     rec = read_fna(file_i)  # go through all files
-        #     dic_template["bacteria"] = bacteria_name
-        #     dic_template["fna"] = fna_name
-        #     dic_template["len_genome"] = len(rec)
-        #     success_n, kmer_counts = \
-        #         count_kmers(rec, dic_template, k, bacteria_name, fna_name, w=window)
-        #     succ_fail = "Success" if len(rec) - 3 == success_n else "Fail   "
-        #     #                     print(f"{succ_fail} -> Bacteria: {bacteria_name},\t file: {fna_name},\t len: {len(rec)}")
-        #     nucleotides.append(success_n)
-        #
-        #     bac_kmers.extend(kmer_counts)
-        # except Exception as e:
-        # print("type error: " + str(e))
-        # #                     print(traceback.format_exc())
-        # print(file_i.path)
-        #
-        # if len(bac_kmers) > 0:
-        #     # Pandas
-        #     df = to_pandas(bac_kmers, bacteria_name)
-        #     # Save to a file
-        #     df.to_pickle(kmer_freq_path)
-        #     n += 1
-
-    # Ending
-    elapsed_time = time() - start
-    print(f"\n{FilesInDir.file_count_root} folders have been scanned\n"
-          f"Took {elapsed_time:,.1f}s / {elapsed_time / 60:.1f}min  to complete. "
-          f"{FilesInDir.file_count_root / elapsed_time:,.0f} genome/s")
-
-
+# Deprecated method
 def count_all(folder_kmers, scanning=PATHS.RefSeq_DB, k=4, window=1000, stop=3, skip={}):
     start = time()
     n = 0
@@ -213,44 +328,3 @@ def count_all(folder_kmers, scanning=PATHS.RefSeq_DB, k=4, window=1000, stop=3, 
     print(f"\n{n} folders have been scanned\n"
           f"Took {elapsed_time:,.1f}s / {elapsed_time / 60:.1f}min  to complete. {total / elapsed_time:,.0f} bp/s")
     return nucleotides_counts
-
-
-def find_bins_DB(folder_kmer_counts, n_parts=10):
-    """ Given a database of genomes in fastq files, split it in n segments """
-    NotImplementedError
-
-
-def combine_genome_kmer_counts():
-    raise NotImplementedError
-
-
-def main(folder_database, folder_intermediate_files, n_clusters, cores):
-    """ Pre-processing of RefSeq database to split genomes into windows, then count their k-mers
-        Second part, load all the k-mer counts into one single Pandas dataframe
-        Third train a clustering algorithm on the k-mer frequencies of these genomes' windows
-    """
-    scan_RefSeq_to_kmer_counts(folder_database, folder_intermediate_files, )
-    combine_genome_kmer_counts(folder_intermediate_files)
-    find_bins_DB(folder_intermediate_files)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=
-        'Take a database of genomes and split it into bins according to the k-mer frequency of each genome. ' \
-        'Needs a lot of disk space. Build the part for a specified classifier if specified')
-    parser.add_argument('path_database', type=is_valid_directory,
-                        help='Database root folder. Support format: RefSeq 2019.')
-    parser.add_argument('path_intermediate_files', type=is_valid_directory,
-                        help='Folder for the k-mer counts per genome and for clustering models')
-    parser.add_argument('-c', '--clusters', default=10, type=int, help='Number of clusters to split the DB into')
-    parser.add_argument('-t', '--threads',  default="10", help='Number of threads')
-    args = parser.parse_args()
-
-    main(folder_database=args.path_database, folder_intermediate_files=args.path_intermediate_files,
-         n_clusters=args.clusters, cores=args.threads)
-    print("Not implemented yet")
-        
-
-
-
-
