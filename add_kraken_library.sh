9d2024b3e3e7afaa0e3a589ce73325f446cb30ff
Earwelen <sylvainriondet@gmail.com>
2020-01-17 16:09:10 +0800

    documentation + starting from 0

--- add_kraken_library.sh
+++ add_kraken_library.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+# Add the genome segments to kraken libraries before building the hash table
+
 cores=2
-for cluster in {1..9}
+for cluster in {0..9}
 do
 find ~/Disks/SSD500/Segmentation/Kraken_10_clusters_V1/$cluster/*/ -name '*.fa' -print0 | xargs -0 -I{} -n1 -P$cores kraken2-build --add-to-library {} --db /home/ubuntu/Disks/SSD500/Segmentation/Kraken_10_clusters_V1/Kraken2_building/$cluster/
 done
