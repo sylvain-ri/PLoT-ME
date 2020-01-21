52b40a71c9543f2bc6ad048a3767bd67142fc38b
Sylvain <sylvainriondet@gmail.com>
2020-01-22 02:25:53 +0800

    trying to suppress stdout

--- prod/parse_DB.py
+++ prod/parse_DB.py
@@ -392,7 +392,8 @@ def kraken_build(path_refseq_binned, path_bins_hash, n_clusters):
                "xargs", "-0", "-I{}", "-n1", "-P", f"{main.cores}",
                "kraken2-build", "--add-to-library", "{}", "--db", osp.join(path_bins_hash, str(cluster))]
         logger.info(f"kraken2 add to library, bin {cluster}, cmd: " + " ".join(cmd))
-        res = subprocess.run(cmd, shell=True, check=True, stdout=os.devnull)
+        with open(os.devnull, 'w') as nothing:
+            res = subprocess.run(cmd, shell=True, check=True, stdout=nothing)
         logger.debug(res)
         # with Pool(min(4, main.cores)) as pool:  # file copy don't need many cores (main.cores)
         #     list_files = os.listdir(osp.join(path_refseq_binned, str(cluster)))
