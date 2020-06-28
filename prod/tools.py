b1ddcabd02e2be1136e3b7252117466f7c95378c
Sylvain <sylvainriondet@gmail.com>
2020-06-28 03:30:03 +0200

    in case of NA values.

--- prod/tools.py
+++ prod/tools.py
@@ -229,11 +229,15 @@ def scale_df_by_length(data, kmer_cols, k, w, single_row=False, ):
 
         # Vertical vector of ratios
         ratio = 4**k / data[kmer_cols].sum(axis=1)
-        logger.debug(f"scaling ratio is {ratio}, from w={w} and k={k}")
+        logger.debug(f"scaling ratio is {ratio.iloc[0,0]}, from w={w} and k={k}")
 
         # Scale by column
         for col in kmer_cols:
             data[col] *= ratio
+
+        if data[kmer_cols].isnull().any(axis=1).any():
+            data[kmer_cols].interpolate(axis='columns', inplace=True)
+            data[kmer_cols].fillna(method="ffill", inplace=True).fillna(method="bfill", inplace=True)
 
 
 class ScanFolder:
