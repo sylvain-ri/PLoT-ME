8edc23d6573b377b0943a5c81b0cda7b63420087
Sylvain <sylvainriondet@gmail.com>
2021-04-15 12:36:38 +0200

    adding the init.py for the module

---
+++ plot_me/cyt_ext/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+#############################################################################
+Package script, for Cython module
+
+#############################################################################
+Sylvain @ GIS / Biopolis / Singapore
+Sylvain Jun-Zhe RIONDET <Riondet_Sylvain_from.tp@gis.a-star.edu.sg>
+Started on 2019-12-11
+Reads Binning Project
+#############################################################################
+"""
+
+if __name__ == '__main__':
+    print(f"PLoT-ME's Cython accelerator")
+
