93d36576330b002b068c0ff10325cfa796d5f2e5
Sylvain <sylvainriondet@gmail.com>
2021-04-16 11:59:54 +0200

    automated even more tha building. fixing wrong wheel name

--- build_test.sh
+++ build_test.sh
@@ -1,7 +1,13 @@
 # Uninstall previous PLoT-Me, build the new one, change the wheel for all linux, install it
 python3 -m pip uninstall -y PLoT-ME
 python3 -m build
-wheel_name=$(find -t dist/*.whl | head -1)
+dist_name=$(ls -t dist/*.whl | head -1)
+echo "dist_name"
+auditwheel repair --plat manylinux2014_x86_64 "dist_name"
+wheel_name=$(ls -t wheelhouse/*.whl | head -1)
 echo "$wheel_name"
-auditwheel repair --plat manylinux2014_x86_64 "$wheel_name"
 python3 -m pip install "$wheel_name"
+
+path_DB="/mnt/data/PLoT-ME-DB"
+rm -r "$path_DB/k4_s10000/"
+echo "plot-me.preprocess /mnt/data/NCBI/20190704/refseq /mnt/data/taxonomy $path_DB"
