b5c0db87a846e98f06c2afd616d2f33a929fa32c
Sylvain <sylvainriondet@gmail.com>
2021-04-20 04:19:00 +0200

    data should be float. adding another test case

--- testing/test_plot_me.py
+++ testing/test_plot_me.py
@@ -2,7 +2,7 @@
 """
 First attempt to add test to project, using pytest
 python3 -m pytest -v
-python3 -m pytest -v --color=yes --log-level=0 | less -r
+python3 -m pytest -v --color=yes --log-level=5 | less -r
 
 Testing both plot_me.bio and plot_me.cython_module.cyt_ext
 """
@@ -96,8 +96,8 @@ initialized_data = [
     (2,  {"AA": "TT", "CA": "TG", "GA": "TC", "TA": "TA", "AC": "GT", "CC": "GG", "GC": "GC", "TC": "GA",
           "AG": "CT", "CG": "CG", "GG": "CC", "TG": "CA", "AT": "AT", "CT": "AG", "GT": "AC", "TT": "AA", },
          # dic with only the kept k-mers
-         {"AA": 0, "CA": 0, "GA": 0, "TA": 0, "AC": 0, "CC": 0, "GC": 0,
-          "AG": 0, "CG": 0, "AT": 0, },
+         {"AA": 0., "CA": 0., "GA": 0., "TA": 0., "AC": 0., "CC": 0., "GC": 0.,
+          "AG": 0., "CG": 0., "AT": 0., },
          # mapping of array indexes, for the forward strand, FROM where it should be added
          # AA, CA, GA, TA, AC, CC, GC, AG, CG, AT
          [  0,  4,  8, 12,  1,  5,  9,  2,  6,  3, ],
@@ -128,8 +128,8 @@ def test_codons_without_rev_comp():
 # #############   KMER COUNTER   #######################
 seq_w_expected_counts = [
     {'k':2, 'seq': "AAAACCCCGGGGTTTT",
-     'counts': {"AA": 3, "CA": 0, "GA": 0, "TA": 0, "AC": 1, "CC": 3, "GC": 0, "TC": 0,
-                "AG": 0, "CG": 1, "GG": 3, "TG": 0, "AT": 0, "CT": 0, "GT": 1, "TT": 3, }, },
+     'counts': {"AA": 3., "CA": 0., "GA": 0., "TA": 0., "AC": 1., "CC": 3., "GC": 0., "TC": 0.,
+                "AG": 0., "CG": 1., "GG": 3., "TG": 0., "AT": 0., "CT": 0., "GT": 1., "TT": 3., }, },
     {'k':3, 'seq': "AAAACCCCGGGGTTTT",
      'counts': {"AAA":2., "CAA":0., "GAA":0., "TAA":0., "ACA":0., "CCA":0., "GCA":0., "TCA":0., "AGA":0., "CGA":0., "GGA":0., "TGA":0., "ATA":0., "CTA":0., "GTA":0., "TTA":0.,
                 "AAC":1., "CAC":0., "GAC":0., "TAC":0., "ACC":1., "CCC":2., "GCC":0., "TCC":0., "AGC":0., "CGC":0., "GGC":0., "TGC":0., "ATC":0., "CTC":0., "GTC":0., "TTC":0.,
@@ -163,10 +163,10 @@ def test_cyt_seq_count_kmer_return_dict(k, seq, counts, np_counts):
 # ######################    TESTING COMBINATION FOR FORWARD AND REVERSE COMPLEMENT    ######################
 # Combining forward and reverse into one array / list
 counts_all_to_combined = [
-    ({"AA": 2, "CA": 2, "GA": 5, "TA": 9, "AC": 4, "CC": 3, "GC": 6, "TC": 1,
-      "AG": 6, "CG": 4, "GG": 7, "TG": 2, "AT": 1, "CT": 5, "GT": 8, "TT": 3, },
-     {"AA": 5, "AC":12, "AG":11, "AT": 1, "CA": 4, "CC":10, "CG": 4,
-      "GA": 6, "GC": 6, "TA": 9, },
+    ({"AA": 2., "CA": 2., "GA": 5., "TA": 9., "AC": 4., "CC": 3., "GC": 6., "TC": 1.,
+      "AG": 6., "CG": 4., "GG": 7., "TG": 2., "AT": 1., "CT": 5., "GT": 8., "TT": 3., },
+     {"AA": 5., "AC":12., "AG":11., "AT": 1., "CA": 4., "CC":10., "CG": 4.,
+      "GA": 6., "GC": 6., "TA": 9., },
      2),
 ]
 np_counts_all_to_combined = []
@@ -190,11 +190,16 @@ def test_cyt_combine_counts_with_reverse_complement(array_counts, array_combined
 # Count k-mers in sequence And combine them
 seq_combined_k_mers = [
     (2, "GGGGGGGGGGGGGGGGGGGGG",
-     {"AA": 0, "AC": 0, "AG": 0, "AT": 0, "CA": 0, "CC": 20, "CG": 0,
-      "GA": 0, "GC": 0, "TA": 0, }),
+     {"AA": 0., "AC": 0., "AG": 0., "AT": 0., "CA": 0., "CC": 20., "CG": 0.,
+      "GA": 0., "GC": 0., "TA": 0., }),
     (2, "TTTTTTTTTTTGGGGCCC",
-     {"AA":10, "AC": 0, "AG": 0, "AT": 0, "CA": 1, "CC": 5, "CG": 0,
-      "GA": 0, "GC": 1, "TA": 0, }),
+     {"AA":10., "AC": 0., "AG": 0., "AT": 0., "CA": 1., "CC": 5., "CG": 0.,
+      "GA": 0., "GC": 1., "TA": 0., }),
+    (3, "TTTTTTTTTTTGGGGCCCGAAT",
+    {"AAA": 9., "AAC": 0., "AAG": 0., "AAT": 1., "ACA": 0., "ACC": 0., "ACG": 0., "ACT": 0., "AGA": 0., "AGC": 0., "AGG": 0., "AGT": 0., "ATA": 0., "ATC": 0., "ATG": 0., "ATT": 0.,
+     "CAA": 1., "CAC": 0., "CAG": 0., "CAT": 0., "CCA": 1., "CCC": 3., "CCG": 1., "CCT": 0., "CGA": 1., "CGC": 0., "CGG": 0., "CGT": 0., "CTA": 0., "CTC": 0., "CTG": 0., "CTT": 0.,
+     "GAA": 1., "GAC": 0., "GAG": 0., "GAT": 0., "GCA": 0., "GCC": 2., "GCG": 0., "GCT": 0., "GGA": 0., "GGC": 0., "GGG": 0., "GGT": 0., "GTA": 0., "GTC": 0., "GTG": 0., "GTT": 0.,
+     "TAA": 0., "TAC": 0., "TAG": 0., "TAT": 0., "TCA": 0., "TCC": 0., "TCG": 0., "TCT": 0., "TGA": 0., "TGC": 0., "TGG": 0., "TGT": 0., "TTA": 0., "TTC": 0., "TTG": 0., "TTT": 0., })
 ]
 seq_combined_k_mers_array = []
 for item in seq_combined_k_mers:
@@ -207,11 +212,11 @@ def test_bio_count_and_combine_seq(k, seq, counts):
 
 @pytest.mark.parametrize("k, seq, counts", seq_combined_k_mers)
 def test_cyt_count_and_combine_seq_dicts(k, seq, counts):
-    assert cyt_ext.kmer_counter(seq, k=k, dictionary=True, combine=True) == counts
+    np.testing.assert_array_equal(counts, cyt_ext.kmer_counter(str.encode(seq), k=k, dictionary=True, combine=True))
 
 @pytest.mark.parametrize("k, seq, counts", seq_combined_k_mers_array)
 def test_cyt_count_and_combine_seq_arrays(k, seq, counts):
-    assert cyt_ext.kmer_counter(seq, k=k, dictionary=False, combine=True) == counts
+    np.testing.assert_array_equal(counts, cyt_ext.kmer_counter(str.encode(seq), k=k, dictionary=False, combine=True))
 
 
 
