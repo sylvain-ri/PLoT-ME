2992e0d51894e4b41d613032f41144f06cc6d5c4
Sylvain <sylvainriondet@gmail.com>
2021-04-21 23:27:21 +0200

    adapting the path for timing records and commenting why the import is later

--- plot_me/__init__.py
+++ plot_me/__init__.py
@@ -15,12 +15,14 @@ Reads Binning Project
 from datetime import datetime
 from pathlib import Path
 
-PLOT_ME_ROOT = Path.home().joinpath("PLoT-ME")
+PLOT_ME_ROOT = Path.home().joinpath("tmp/PLoT-ME")
 LOGS = PLOT_ME_ROOT.joinpath(f"logs/{datetime.now():%Y-%m-%d_%H-%M}.log")
 LOGS.parent.mkdir(parents=True, exist_ok=True)
-RECORDS = PLOT_ME_ROOT.joinpath(f"logs/classify_timings.tsv")
+RECORDS = PLOT_ME_ROOT.joinpath(f"timings/classify_{datetime.now():%Y-%m-%d_%H-%M}.tsv")
 
+# !!  Need to initialize variables before importing modules  !!
 from plot_me import parse_DB, classify, tools, bio
+
 
 if __name__ == '__main__':
     print(f"PLoT-ME : Pre-classification of Long-reads for Memory Efficient Taxonomic assignment "
