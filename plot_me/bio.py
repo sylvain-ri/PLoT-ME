cc75d8c2a34a85a39fca658ffb563933edb20619
Sylvain <sylvainriondet@gmail.com>
2021-04-22 03:00:25 +0200

    extra space removed

--- plot_me/bio.py
+++ plot_me/bio.py
@@ -41,7 +41,7 @@ def codon_addr(codon):
     return total
 
 
-def n_dim_rc_combined( k):
+def n_dim_rc_combined(k):
     """ Return the number of dimensions, for a given k, for the unique k-mer counts (forward - reverse complement) """
     return 2**k + (4**k - 2**k)//2 if k % 2 == 0 else 4**k // 2
 
